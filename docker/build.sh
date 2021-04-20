a358b01d643eb4712029d2695dd39e0a84ce26d8
silas <s.developer@4-dc.de>
2021-04-20 17:55:42 +0200

    Started with CI

--- docker/build.sh
+++ docker/build.sh
@@ -2,7 +2,6 @@
 
 git clone https://edugit.org/sdcieo0330/LibAVQt.git
 
-cd LibAVQt
-mkdir build && cd build
+cd LibAVQt || exit
+mkdir build && (cd build || exit)
 cmake -DCMAKE_C_COMPILER=/usr/bin/gcc-10 -DCMAKE_CXX_COMPILER=/usr/bin/g++-10 -DCMAKE_BUILD_TYPE=Debug .. && cmake --build . -j4
-
